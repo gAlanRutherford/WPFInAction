dfd030f5da77966a759f5efe01849366635554d4
gar35 <grutherford@hotmail.co.nz>
2016-11-06 12:55:05 +1100

    Chapter 2

---
+++ Chapter2/GoodbyeWorld/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace GoodbyeWorld
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+    private void App_OnStartup(object sender, StartupEventArgs e)
+    {
+      this.ShutdownMode = ShutdownMode.OnLastWindowClose;
+    }
+  }
+}
